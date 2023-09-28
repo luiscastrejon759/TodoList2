09dc4de63560a0901e2251f607dac9f4dbe38201
luiscastrejon759 <joseluis.castrejon4@gmail.com>
2023-09-28 15:09:46 -0600

    se agrega el proyecto

---
+++ page.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+export default function Principal(){
+    return(
+    <div className="centrar">
+        <App/>
+    </div>
+    );
+}
